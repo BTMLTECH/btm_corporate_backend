91239ecabe998ca3b5f42fd0a184f301d38509a0
TobiLight <jgetitdonefast@gmail.com>
2024-12-07 01:04:38 +0100

    fix: code change.

--- alembic/env.py
+++ alembic/env.py
@@ -13,11 +13,11 @@ from alembic import context
 
 database_url: str = "{db_engine}://{user}:{password}@{host}:{port}/{database}".format(
     db_engine="postgresql+asyncpg",
-    user=getenv("DB_USER"),
-    password=getenv("DB_PASSWORD"),
-    host=getenv("DB_HOST", "localhost"),
+    user=getenv("DB_LOCAL_USER"),
+    password=getenv("DB_LOCAL_PASSWORD"),
+    host=getenv("DB_LOCAL_HOST", "localhost"),
     port=5432,
-    database=getenv("DB"),
+    database=getenv("DB_LOCAL"),
 )
 
 # this is the Alembic Config object, which provides
