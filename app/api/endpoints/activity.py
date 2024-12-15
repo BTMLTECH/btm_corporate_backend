7985fe2b8c46e8c72f8c823490251f23994cedff
TobiLight <jgetitdonefast@gmail.com>
2024-12-15 08:45:40 +0100

    chore: minor change

--- app/api/endpoints/activity.py
+++ app/api/endpoints/activity.py
@@ -11,7 +11,7 @@ from app.core.exceptions import GeneralError
 from app.model.activity import Activity
 from app.model.user import User
 from app.schema.activity_schema import CreateActivity
-from app.services.region_service import RegionService
+from app.services.activity_service import ActivityService
 from app.core.container import Container
 
 
@@ -24,20 +24,20 @@ router = APIRouter(
 
 @router.post("/add", response_model=Activity)
 @inject
-async def add_activity(region: CreateActivity, service: RegionService = Depends(Provide[Container.region_service]), current_user: User = Depends(is_user_admin)):
+async def add_activity(activity: CreateActivity, service: ActivityService = Depends(Provide[Container.activity_service]), current_user: User = Depends(is_user_admin)):
     """Route to add an activity"""
-    region = await service.add(region)
+    activity = await service.add(activity)
 
-    return region
+    return activity
 
 
 @router.delete("{activity_id}")
 @inject
-async def delete_activity(activity_id: str, service: RegionService = Depends(Provide[Container.region_service]), current_user: User = Depends(is_user_admin)):
+async def delete_activity(activity_id: str, service: ActivityService = Depends(Provide[Container.activity_service]), current_user: User = Depends(is_user_admin)):
     """Route to delete an activity by ID"""
-    region = await service.delete_by_id(activity_id)
+    activity = await service.delete_by_id(activity_id)
 
-    if not region:
-        raise GeneralError(detail="Region has been deleted or does not exist")
+    if not activity:
+        raise GeneralError(detail="Activity has been deleted or does not exist")
 
-    return region
+    return activity
--- app/schema/activity_schema.py
+++ app/schema/activity_schema.py
@@ -3,9 +3,12 @@
 # Author: Oluwatobiloba Light
 """Activity Schema"""
 
+from typing import Union
 from pydantic import BaseModel
 from app.schema.base_schema import ModelBaseInfo
 
 
 class CreateActivity(BaseModel):
     name: str
+    description: Union[str, None] = None
+    price: float = 0
